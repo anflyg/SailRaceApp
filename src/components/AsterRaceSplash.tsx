1abd053135d031d6045f90569a64506ce4aac7b1
Anders Flygare <anflyg@gmail.com>
2026-05-14 09:35:51 +0200

    Add splash fade out

--- src/components/AsterRaceSplash.tsx
+++ src/components/AsterRaceSplash.tsx
@@ -1,27 +1,36 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import './AsterRaceSplash.css'
 
 interface AsterRaceSplashProps {
   onComplete?: () => void
 }
 
-const COMPLETE_DELAY_MS = 1600
+const LEAVE_DELAY_MS = 1400
+const COMPLETE_DELAY_MS = 1700
 
 export default function AsterRaceSplash({ onComplete }: AsterRaceSplashProps) {
-  useEffect(() => {
-    if (!onComplete) {
-      return undefined
-    }
+  const [isLeaving, setIsLeaving] = useState(false)
 
-    const timeoutId = window.setTimeout(onComplete, COMPLETE_DELAY_MS)
+  useEffect(() => {
+    const leaveTimeoutId = window.setTimeout(() => {
+      setIsLeaving(true)
+    }, LEAVE_DELAY_MS)
+
+    const completeTimeoutId = window.setTimeout(() => {
+      onComplete?.()
+    }, COMPLETE_DELAY_MS)
 
     return () => {
-      window.clearTimeout(timeoutId)
+      window.clearTimeout(leaveTimeoutId)
+      window.clearTimeout(completeTimeoutId)
     }
   }, [onComplete])
 
   return (
-    <div className="aster-race-splash" aria-label="Aster Race start screen">
+    <div
+      className={`aster-race-splash ${isLeaving ? 'aster-race-splash--leaving' : ''}`}
+      aria-label="Aster Race start screen"
+    >
       <div className="aster-race-splash__halo" aria-hidden="true" />
       <div className="aster-race-splash__content">
         <svg
